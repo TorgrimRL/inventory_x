9bfa3bc2d1cc03fd2e1752c6d8bcad3c3a37e4e9
Daniel-De-Dev <id@daniel-de-dev.com>
2026-01-21 17:53:34 +0100

    chore: Removed flake file from project & scripts
    Backend & database is setup and run trough docker. No need for the
    scripts
    
    Found a solution for moving the flake outside of the repo to decrease
    clutter.

--- backend/scripts/db_reset.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-dropdb -h $PGHOST -p $PGPORT $DATABASE_NAME --if-exists --force
-createdb -h $PGHOST -p $PGPORT $DATABASE_NAME
-uv run python manage.py migrate >/dev/null
-uv run python manage.py seed_db
