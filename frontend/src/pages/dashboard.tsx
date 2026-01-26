5fa3fd20c5f6f924efa2ebaa47d033a7dfeff6a8
blackh-t <nache7383@uit.no>
2026-01-26 19:45:59 +0100

    feat(frontend): fully mapped dummy page and form
    These form have been config such that dashbard will render after a
    sucsess login, and registration reder on "create account" button cliked.

---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Inventory Dashboard</h1>
+      <p>If you see this, your login redirect worked!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
