023f4c54683fa90d11e0cea84174836f0f375fc9
blackh-t <nache7383@uit.no>
2026-01-26 19:48:02 +0100

    refactor(backend.user): corrected parameter fieldname

--- backend/api/user/urls.py
+++ backend/api/user/urls.py
@@ -6,7 +6,7 @@ from api.user.views.verify import VerifyView
 from .views.login import LoginView
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), nane="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view()),
     path("verify/", VerifyView.as_view()),
 ]
