8b82ff0bfd723dee8b48d047a5a9e5ad5480067f
blackh-t <nache7383@uit.no>
2026-01-27 11:58:34 +0100

    refactor(backend): moved logou logic from login branch

--- backend/api/user/urls.py
+++ backend/api/user/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from api.user.views.logout import LogoutView
 from api.user.views.verify import VerifyView
 
 from .views.login import LoginView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view()),
     path("verify/", VerifyView.as_view()),
 ]
