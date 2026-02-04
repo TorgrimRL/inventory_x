d520101f51c19aa6b4641a7d727b4b6e071bbd13
blackh-t <nache7383@uit.no>
2026-02-04 15:17:07 +0100

    refactor(frontend): Configured axios to include session in all requests.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
+import axios from "axios";
+
+// Ensure sessions is included in all request method.
+// Browser drops cookies on cross-origin requests.
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
