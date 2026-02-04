8d6e745228e38f146d32cc8f2238d473a6eb6c15
blackh-t <nache7383@uit.no>
2026-02-04 22:21:05 +0100

    FORMATTING

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,17 @@
 import "./index.css";
 
+import axios from "axios";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
-import axios from "axios";
 
 // Ensure sessions is included in all request method.
 // Browser drops cookies on cross-origin requests.
 axios.defaults.withCredentials = true;
-axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
