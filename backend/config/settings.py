a5f5d5a1292f41602a33439179a8df02e26a1f0b
blackh-t <nache7383@uit.no>
2026-02-05 11:07:38 +0100

    refactor: [CORS] Added localhost to origin

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -105,11 +105,13 @@ USE_TZ = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 # CSRF CONFIG
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 CSRF_COOKIE_HTTPONLY = False
 CSRF_COOKIE_SAMESITE = "Lax"
