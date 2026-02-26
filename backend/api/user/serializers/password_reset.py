78508eb34201dcc6a5cb1fe64e6cde1e8663b0bc
blackh-t <nache7383@uit.no>
2026-02-26 13:22:46 +0100

    feat:[+] PASSWORD RESET Serializer and auto validation

---
+++ backend/api/user/serializers/password_reset.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.password_validation import validate_password
+from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
+from rest_framework import serializers
+
+
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            name="Valid Request Example",
+            value={
+                "OTC": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca49599"
+                "1b7852b855",
+                "NEW_PASSWORD": "StrongPassword123!",
+            },
+            request_only=True,
+        )
+    ]
+)
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    OTC = serializers.CharField(
+        help_text="The One-Time Code received via email.",
+        min_length=64,
+        max_length=64,
+    )
+    NEW_PASSWORD = serializers.CharField(
+        min_length=8,
+        style={"input_type": "password"},
+    )
+
+    def validate_NEW_PASSWORD(self, value: str) -> str:
+        # This automatically runs Django's built-in password validators
+        validate_password(value)
+        return value
