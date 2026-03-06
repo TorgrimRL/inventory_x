6abd798bac14599f2e48440cd1a3020fa010f603
blackh-t <nache7383@uit.no>
2026-03-06 11:56:08 +0100

    autoformat

--- backend/api/user/validators.py
+++ backend/api/user/validators.py
@@ -12,7 +12,7 @@ class CustomMinimumLengthValidator:
     def validate(self, password, user=None):
         if len(password) < self.min_length:
             raise ValidationError(
-                f"Password must be at least {self.min_length} characters long.",
+                f"Password must be at least {self.min_length} long.",
                 code="password_too_short",
             )
 
@@ -20,19 +20,21 @@ class CustomMinimumLengthValidator:
 class CustomCommonPasswordValidator(CommonPasswordValidator):
     def validate(self, password, user=None):
         try:
-            # checking its built-in list of 1000 common passwords
             super().validate(password, user)
         except ValidationError:
             raise ValidationError(
-                "This password is too easy to guess. Please choose a more unique password.",
+                "This password is too easy to guess. "
+                "Please choose a more unique password.",
                 code="password_too_common",
-            )
+            ) from None
 
 
 class CustomNumericPasswordValidator(NumericPasswordValidator):
     def validate(self, password, user=None):
         if password.isdigit():
+            # Breaking the string into two lines fixes the 80-character limit
             raise ValidationError(
-                "Your password cannot consist entirely of numbers. Please add some letters or symbols.",
+                "Your password cannot consist entirely of numbers. "
+                "Please add some letters or symbols.",
                 code="password_entirely_numeric",
             )
